02665879ad197b8bd1bbccf3f5cae5847c07d502
chiragdianaapps06 <chirag.trivedi@dianapps.com>
2025-07-10 16:57:39 +0530

    make further efficient data dumping using threading

--- product/scripts/load_data_threading.py
+++ product/scripts/load_data_threading.py
@@ -1,10 +1,11 @@
+
 import csv
 import os
 import logging
 from datetime import datetime
 from django.db import transaction
 from django.conf import settings
-from concurrent.futures import ThreadPoolExecutor, as_completed 
+from concurrent.futures import ThreadPoolExecutor, as_completed
 import threading
 import gc
 
@@ -28,14 +29,21 @@ logger.setLevel(logging.INFO)
 logger.addHandler(logging.StreamHandler())
 
 
+def batch_generator(reader, batch_size=10000):
+    batch = []
+    for row in reader:
+        batch.append(row)
+        if len(batch) == batch_size:
+            yield batch
+            batch = []
+    if batch:
+        yield batch
+
+
 def run():
     csv_file_path = os.path.join(settings.BASE_DIR, 'data', '5m Sales Records.csv')
     batch_size = 10000
-    batch = []
-    count = 0
-   
 
-       
     try:
         with open(csv_file_path, 'r') as file:
             reader = csv.DictReader(file)
@@ -49,30 +57,21 @@ def run():
                 (item.item_type.strip(), float(item.unit_cost)): item
                 for item in Item.objects.all()
             }
+
             existing_orders = {
                 o.order_id: o for o in Order.objects.all()
             }
-            
 
+            c=0
+            for count, batch in enumerate(batch_generator(reader, batch_size)):
+                batch_process(batch, existing_items, existing_countries, existing_orders)
+                # logger.info(f"Processed batch {count + 1}")
 
-            for row in reader:
-                batch.append(row)
-                if len(batch) >= batch_size:
-
-                    batch_process(batch,existing_countries,existing_items,existing_orders )
-                    logger.info(f"Processed batch up to row {count + batch_size}")
-                     
+                logger.info(f"Processed batch up to row {c+ batch_size}")
+                
+                c+=batch_size
                        
-                    count += batch_size
-                    batch = []
-
-            if batch:
-                batch_process(batch,existing_countries,existing_items,existing_orders)
-               
-                logger.info(f"Processed final batch up to row {count + len(batch)}")
-
-            
-          
+#                     count += batch_size
 
     except FileNotFoundError:
         logger.critical(f"CSV file not found at path: {csv_file_path}")
@@ -80,102 +79,78 @@ def run():
         logger.critical(f"Fatal error occurred: {e}")
 
 
-def product_items_process(batch,existing_items,items_to_create):
+def product_items_process(batch, existing_items, items_to_create):
     new_item_keys = set()
-    logger.info(f"[{threading.current_thread().name}] Running item_process on batch of size {len(batch)}")
-
     for row in batch:
         try:
             item_key = (row['Item Type'].strip(), float(row['Unit Cost']))
             if item_key not in existing_items and item_key not in new_item_keys:
-                item = Item(item_type=item_key[0], unit_cost=item_key[1])
-                items_to_create.append(item)
+                items_to_create.append(Item(item_type=item_key[0], unit_cost=item_key[1]))
                 new_item_keys.add(item_key)
         except Exception as e:
             logger.warning(f"Item skip: {e}")
-            continue
-    logger.info(f"[{threading.current_thread().name}] item created in current batch {len(items_to_create)}")
+    logger.info(f"[{threading.current_thread().name}] Items to create: {len(items_to_create)}")
 
-def country_process(batch,existing_countries,countries_to_create):
+
+def country_process(batch, existing_countries, countries_to_create):
     new_country_keys = set()
-
-    logger.info(f"[{threading.current_thread().name}] Running country_process on batch of size {len(batch)}")
     for row in batch:
         try:
             country_key = (row['Country'].strip(), row['Region'].strip())
             if country_key not in existing_countries and country_key not in new_country_keys:
-                country = Country(Country_name=country_key[0], region=country_key[1])
-                countries_to_create.append(country)
+                countries_to_create.append(Country(Country_name=country_key[0], region=country_key[1]))
                 new_country_keys.add(country_key)
         except Exception as e:
             logger.warning(f"Country skip: {e}")
-            continue
-    logger.info(f"[{threading.current_thread().name}] cuntries created in current bacth {len(countries_to_create)}")
+    logger.info(f"[{threading.current_thread().name}] Countries to create: {len(countries_to_create)}")
 
 
-def order_process(batch,existing_items,existing_countries,existing_orders,orders_to_create):
-
-    logger.info(f"[{threading.current_thread().name}] Running order_process on batch of size {len(batch)}")
-
-    new_order_keys = set()
+def order_process(batch, existing_items, existing_countries, existing_orders, orders_to_create):
     for row in batch:
         try:
             item_key = (row['Item Type'].strip(), float(row['Unit Cost']))
             country_key = (row['Country'].strip(), row['Region'].strip())
+            order_id = row['Order ID'].strip()
+
             item = existing_items.get(item_key)
             country = existing_countries.get(country_key)
-            if not item or not country:
-                continue
-            order_id = row['Order ID'].strip()
-          
 
-            if str(order_id) not in  existing_orders:
-                order = Order(
-                    order_id=row['Order ID'].strip(),
-                    item_type=item,
-                    order_date=datetime.strptime(row['Order Date'], '%m/%d/%Y'),
-                    ship_date=datetime.strptime(row['Ship Date'], '%m/%d/%Y'),
-                    country=country,
-                    order_priority=row['Order Priority'].strip(),
-                    unit_sold=int(row['Units Sold']),
-                    unit_price=float(row['Unit Price']),
-                    total_price=float(row['Total Revenue']),
-                )
-                orders_to_create.append(order)
+            if not item or not country or order_id in existing_orders:
+                continue
+
+            orders_to_create.append(Order(
+                order_id=order_id,
+                item_type=item,
+                order_date=datetime.strptime(row['Order Date'], '%m/%d/%Y'),
+                ship_date=datetime.strptime(row['Ship Date'], '%m/%d/%Y'),
+                country=country,
+                order_priority=row['Order Priority'].strip(),
+                unit_sold=int(row['Units Sold']),
+                unit_price=float(row['Unit Price']),
+                total_price=float(row['Total Revenue']),
+            ))
         except Exception as e:
             logger.warning(f"Order skip: {e}")
-            continue
-    logger.info(f"[{threading.current_thread().name}] order created in current batch {len(orders_to_create)}")
+    logger.info(f"[{threading.current_thread().name}] Orders to create: {len(orders_to_create)}")
 
 
-    
-
-def sale_process(batch,existing_orders,existing_countries,sales_to_create):
+def sale_process(batch, existing_orders, existing_countries, sales_to_create):
 
 
-    
-    order_ids = [r['Order ID'].strip() for r in batch ]
-
-    
-    existing_orders.update({
-        o.order_id: o
-        for o in Order.objects.filter(order_id__in=order_ids)
-        
-    })
-
-
-
-    logger.info(f"[{threading.current_thread().name}] Running sales process on batch of size {len(batch)}")
     for row in batch:
         try:
-       
             order = existing_orders.get(int(row['Order ID'].strip()))
-            country = existing_countries.get((row['Country'].strip(), row['Region'].strip()))
-   
+            country_key = (row['Country'].strip(), row['Region'].strip())
+            country = existing_countries.get(country_key)
+
+        
             if not order or not country:
                 continue
+
            
-            sale = Sales(
+            
+
+            sales_to_create.append(Sales(
                 order_id=order,
                 sales_channel=row['Sales Channel'].strip(),
                 country=country,
@@ -183,87 +158,61 @@ def sale_process(batch,existing_orders,existing_countries,sales_to_create):
                 total_cost=float(row['Total Cost']),
                 total_price=float(row['Total Revenue']),
                 total_revenue=float(row['Total Profit']),
-            )
-            sales_to_create.append(sale)
+            ))
+            
         except Exception as e:
             logger.warning(f"Sale skip: {e}")
-            continue
-    logger.info(f"[{threading.current_thread().name}] sales created in current bacth {len(sales_to_create)}")
+    logger.info(f"[{threading.current_thread().name}] Sales to create: {len(sales_to_create)}")
 
 
-
-def batch_process(batch,existing_items, existing_countries,existing_orders):
-  
-
-    orders_to_create = []
-    sales_to_create = []
+def batch_process(batch, existing_items, existing_countries, existing_orders):
     items_to_create = []
     countries_to_create = []
+    orders_to_create = []
+    sales_to_create = []
 
     try:
         with transaction.atomic():
-           
-
             with ThreadPoolExecutor(max_workers=4) as executor:
-                futures = []
-                futures.append(executor.submit(product_items_process,batch,existing_items,items_to_create))
-                for future in futures:
+                futures = [
+                    executor.submit(product_items_process, batch, existing_items, items_to_create),
+                    executor.submit(country_process, batch, existing_countries, countries_to_create),
+                    executor.submit(order_process, batch, existing_items, existing_countries, existing_orders, orders_to_create),
+                ]
+                for future in as_completed(futures):
                     future.result()
 
             Item.objects.bulk_create(items_to_create, batch_size=2000, ignore_conflicts=True)
+            Country.objects.bulk_create(countries_to_create, batch_size=2000, ignore_conflicts=True)
+            Order.objects.bulk_create(orders_to_create, batch_size=2000, ignore_conflicts=True)
+            
+            Sales.objects.bulk_create(sales_to_create, batch_size=2000, ignore_conflicts=True)
 
             existing_items.update({
                 (item.item_type.strip(), float(item.unit_cost)): item
                 for item in Item.objects.filter(item_type__in=[r['Item Type'].strip() for r in batch])
             })
 
-            with ThreadPoolExecutor(max_workers=4) as executor:
-                futures =[]
-                futures.append(executor.submit(country_process,batch,existing_countries,countries_to_create))
-
-                for future in futures:
-                    future.result()
-            Country.objects.bulk_create(countries_to_create, batch_size=2000, ignore_conflicts=True)
-
-
             existing_countries.update({
                 (c.Country_name.strip(), c.region.strip()): c
                 for c in Country.objects.filter(Country_name__in=[r['Country'].strip() for r in batch])
             })
 
-            with ThreadPoolExecutor(max_workers=4) as executor:
-                futures =[]
-                futures.append(executor.submit(order_process,batch,existing_items,existing_countries,existing_orders,orders_to_create))
-
-                for future in futures:
-                    future.result()
-
-            Order.objects.bulk_create(orders_to_create, batch_size=2000, ignore_conflicts=True)
-
-
-            order_ids = [r['Order ID'].strip() for r in batch ]
-
-            
             existing_orders.update({
-                o.order_id: o
-                for o in Order.objects.filter(order_id__in=order_ids)
-                
+                o.order_id: o for o in Order.objects.filter(order_id__in=[r['Order ID'].strip() for r in batch])
             })
 
-            
+            with ThreadPoolExecutor(max_workers=2) as executor:
+              futures = [
+                  executor.submit(sale_process,batch, existing_orders, existing_countries, sales_to_create)
+              ]
+              for future in futures:
+                  future.result()
 
-            
 
-            with ThreadPoolExecutor(max_workers=4) as executor:
-                futures =[]
-                futures.append(executor.submit(sale_process,batch,existing_orders,existing_countries,sales_to_create))
 
-                for future in futures:
-                    future.result()
-           
-
+            # sale_process(batch, existing_orders, existing_countries, sales_to_create)
             Sales.objects.bulk_create(sales_to_create, batch_size=2000, ignore_conflicts=True)
-
 
 
             # Clear memory
@@ -275,10 +224,9 @@ def batch_process(batch,existing_items, existing_countries,existing_orders):
             gc.collect()
 
     except Exception as e:
-        logger.warning(f"Batch failed: {e} ")
+        logger.warning(f"Batch failed: {e}")
 
 
 if __name__ == '__main__':
     run()
-
 
